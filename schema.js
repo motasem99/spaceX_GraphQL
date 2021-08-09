084a19fd3f7be5d44d33783799f58f0d42132df2
mutasem <motasemimad10@gmail.com>
2021-08-09 15:37:18 +0300

    add root query to get all launch

--- schema.js
+++ schema.js
@@ -1,8 +1,12 @@
+const axios = require('axios');
+
 const {
   GraphQLObjectType,
   GraphQLInt,
   GraphQLString,
   GraphQLBoolean,
+  GraphQLList,
+  GraphQLSchema,
 } = require('graphql');
 
 // Launch Type
@@ -12,7 +16,7 @@ const LaunchType = new GraphQLObjectType({
     flight_number: { type: GraphQLInt },
     mission_name: { type: GraphQLString },
     launch_year: { type: GraphQLString },
-    launch_date_local: { type: GraphQLInt },
+    launch_date_local: { type: GraphQLString },
     launch_success: { type: GraphQLBoolean },
     rocket: { type: RocketType },
   }),
@@ -26,4 +30,23 @@ const RocketType = new GraphQLObjectType({
     rocket_name: { type: GraphQLString },
     rocket_type: { type: GraphQLString },
   }),
+});
+
+// RootQuery
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQuery',
+  fields: {
+    launches: {
+      type: new GraphQLList(LaunchType),
+      resolve(parent, args) {
+        return axios
+          .get('https://api.spacexdata.com/v3/launches')
+          .then((res) => res.data);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
