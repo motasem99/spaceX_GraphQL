8a908600be78948a156ebaed289a52aa5013c299
mutasem <motasemimad10@gmail.com>
2021-08-09 17:42:08 +0300

    install cors to display data and get data launches

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 // import ApolloClient from 'apollo-boost';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import Logo from './logoSpaceX.png';
+import Launches from './components/launches/Launches.jsx';
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
@@ -17,6 +18,7 @@ function App() {
           alt='SpaceX'
           style={{ width: 300, display: 'block', margin: 'auto' }}
         />
+        <Launches />
       </div>
     </ApolloProvider>
   );
---
+++ client/src/components/launches/Launches.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+// import gql from 'graphql-tag';
+import { useQuery, gql } from '@apollo/client';
+
+const LAUNCHES_QUERY = gql`
+  query LaunchesQuery {
+    launches {
+      flight_number
+      mission_name
+      launch_date_local
+      launch_success
+    }
+  }
+`;
+
+const Launches = () => {
+  const { loading, error, data } = useQuery(LAUNCHES_QUERY);
+  if (loading) return <p>Loading</p>;
+  if (error) return <p>Error</p>;
+  console.log(data);
+  return (
+    <div>
+      <h1 className='display-4 my-3'> Launches</h1>
+    </div>
+  );
+};
+
+export default Launches;
