41f8fc4e764814742bf63d586d70ea6f3c9e9fd9
mutasem <motasemimad10@gmail.com>
2021-08-09 16:37:46 +0300

    clear code and setup page spaceX

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from './logoSpaceX.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <img
+        src={Logo}
+        alt='SpaceX'
+        style={{ width: 300, display: 'block', margin: 'auto' }}
+      />
     </div>
   );
 }
