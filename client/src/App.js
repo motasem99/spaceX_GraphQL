51435c804dfba98d60f95aa93386baf1513d082a
mutasem <motasemimad10@gmail.com>
2021-08-09 17:08:48 +0300

    setup spollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
+// import ApolloClient from 'apollo-boost';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import Logo from './logoSpaceX.png';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className='App'>
-      <img
-        src={Logo}
-        alt='SpaceX'
-        style={{ width: 300, display: 'block', margin: 'auto' }}
-      />
-    </div>
+    <ApolloProvider client={client}>
+      <div className='container'>
+        <img
+          src={Logo}
+          alt='SpaceX'
+          style={{ width: 300, display: 'block', margin: 'auto' }}
+        />
+      </div>
+    </ApolloProvider>
   );
 }
 
