9b37941c33341185cd606b2ff052d73c3db3939a
mutasem <motasemimad10@gmail.com>
2021-08-11 13:49:48 +0300

    display details single launch and styling it in launch page

--- client/src/components/Launch/Launch.jsx
+++ client/src/components/Launch/Launch.jsx
@@ -30,13 +30,47 @@ const Launch = () => {
   });
   if (loading) return <p>Loading</p>;
   if (error) return <p>Error :(</p>;
+
+  const {
+    mission_name,
+    flight_number,
+    launch_year,
+    launch_success,
+    rocket: { rocket_id, rocket_name, rocket_type },
+  } = data.launch;
   return (
     <Fragment>
       <div>
         <h1 className='display-4 my-3'>
           <span className='text-dark'>Mission: </span>
-          {data.launch.mission_name}
+          {mission_name}
         </h1>
+        <h4 className='mb-3'>Launch Details</h4>
+        <ul className='list-group'>
+          <li className='list-group-item'>flight_number: {flight_number}</li>
+          <li className='list-group-item'>Launch Year: {launch_year}</li>
+          <li className='list-group-item'>
+            Launch Successful:{' '}
+            <span
+              className={classnames({
+                'text-success': launch_success,
+                'text-danger': !launch_success,
+              })}
+            >
+              {launch_success ? 'Yse' : 'No'}
+            </span>
+          </li>
+        </ul>
+        <h4 className='my-3'>Rocket Details</h4>
+        <ul className='list-group'>
+          <li className='list-group-item'>RocketId: {rocket_id}</li>
+          <li className='list-group-item'>Rocket Name: {rocket_name}</li>
+          <li className='list-group-item'>Rocket Type{rocket_type}</li>
+        </ul>
+        <hr />
+        <Link to='/' className='btn btn-secondary'>
+          Back
+        </Link>
       </div>
     </Fragment>
   );
